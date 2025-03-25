3f246e9f0dfe310adaa740a5a6f0ca86828026d1
Sannidhii15 <sannidhis@netanalytiks.com>
2025-03-25 09:50:51 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1074,7 +1074,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 async function loadOrchestrationFile() {
     console.log('Loading Orchestration File...');
     try {
-        const response = await fetch('http://127.0.0.1:5000/server/get_orchestration');
+        const response = await fetch('https://netx.eastus.cloudapp.azure.com/server/get_orchestration');
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
